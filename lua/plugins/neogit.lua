d1961c1868ccb248e108e616d183cce49f781fec
EmilAnkerstjerne <70580136+EmilAnkerstjerne@users.noreply.github.com>
2026-01-29 18:52:03 +0100

    first commit

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = "Neogit",
+    keys = { { "<leader>gg", "<cmd>Neogit<cr>", desc = "Open Neogit" } },
+  }
+}
