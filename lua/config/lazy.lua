1d548784c962e436bb1b3fcae8f79acf15ae2b19
EmilAnkerstjerne <70580136+EmilAnkerstjerne@users.noreply.github.com>
2026-03-03 19:12:41 +0100

    msg

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,7 +24,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.nix" },
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
