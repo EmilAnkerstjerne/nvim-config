c66bfcd9d5e086286f60b33eecffbf173c229fb8
Emil Ankerstjerne <ea@abtion.com>
2026-04-04 22:26:21 +0200

    Prettier default formatter

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,12 @@
+return {
+  "stevearc/conform.nvim",
+  opts = function(_, opts)
+    opts.formatters_by_ft = opts.formatters_by_ft or {}
+    opts.formatters_by_ft.typescript = { "prettier" }
+    opts.formatters_by_ft.typescriptreact = { "prettier" }
+    opts.formatters_by_ft.javascript = { "prettier" }
+    opts.formatters_by_ft.javascriptreact = { "prettier" }
+    opts.formatters_by_ft.json = { "prettier" }
+    return opts
+  end,
+}
